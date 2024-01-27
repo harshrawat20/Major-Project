19d6d99d8a700ab96e4edad1a1093b8c3c18d5ef
Harsh Rawat <20bcs050@iiitdwd.ac.in>
2024-01-27 15:49:28 +0530

    Update functionality integrated in backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,33 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const userRoute=require("./routes/userRoute")
+const errorHandler=require('./MiddleWare/errorMiddleware')
+const cookieParser= require('cookie-parser')
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+// Middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes Middleware
+app.use("/api/users",userRoute)
+
+
+// Routes
+app.get("/",(req,res)=>{
+   res.send("Home Page") 
+})
+
+// Error MiddleWare
+app.use(errorHandler);
 
 // Connect to DB and Start the server
+const PORT = process.env.PORT || 5000
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
